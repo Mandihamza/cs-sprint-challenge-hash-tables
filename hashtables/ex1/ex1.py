1f57c1dd6cbeabe8f9f127ef7dae4e4f12499127
mandihamza <mandihamza@gmail.com>
2020-10-15 15:36:49 -0600

    Solves merge two packages question

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,10 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    num_pairs_dict = {}
+    for index, value in enumerate(weights):
+        if value in num_pairs_dict:
+            tuple_list = tuple([num_pairs_dict[value], index])
+            min_value = min(tuple_list)
+            max_value = max(tuple_list)
+            return max_value, min_value
+        num_pairs_dict[limit - value] = index
     return None
